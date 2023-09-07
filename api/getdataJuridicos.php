e8cbc8e40b914d0873188c7c9a14203ae34601a0
Jônatas Tovani <98124177+jonatastovani@users.noreply.github.com>
2023-09-07 10:29:02 -0300

    Projeto Limpo

--- api/getdataJuridicos.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	require_once( "../env.php" ) ;	
-
-	require_once( "../controllers/DataController.php" ) ;	
-
-	echo DataController::getDataGeneric( $_REQUEST['param'], $url_api, $_REQUEST['type'] );
-
-?>
